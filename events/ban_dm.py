6325a8e240002af5fdf5d06f188a4bd8aedbad63
divyanshbhattacharjee24-netizen <divyanshbhattacharjee24@gmail.com>
2026-06-16 22:29:54 +0530

    Create ban_dm.py
    Add automatic DM when user is banned

---
+++ events/ban_dm.py
@@ -0,0 +1,13 @@
+import discord
+
+async def send_ban_dm(user: discord.User):
+    message = (
+        "You've Been Banned from Roblox Fans Server for breaking the rules.\n\n"
+        "You may reappeal and get unbanned by joining this server and appealing your ban:\n"
+        "https://discord.gg/jyuC9nuFST"
+    )
+
+    try:
+        await user.send(message)
+    except Exception as e:
+        print(f"Failed to DM banned user {user}: {e}")
