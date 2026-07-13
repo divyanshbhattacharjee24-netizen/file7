38a196b987c7f1210be7a1b63aa32d9a8fbf46a1
divyanshbhattacharjee24-netizen <divyanshbhattacharjee24@gmail.com>
2026-07-13 21:02:46 +0530

    Update ban_dm.py

--- events/ban_dm.py
+++ events/ban_dm.py
@@ -3,7 +3,7 @@ import discord
 async def send_ban_dm(user: discord.User):
     message = (
         "You've Been Banned from Roblox Fans Server for breaking the rules.\n\n"
-        "You may reappeal and get unbanned by joining this server and appealing your ban:\n"
+        "You may reappeal and get unbanned by joining this server and appealing your ban https://discord.gg/jyuC9nuFST:\n"
         "https://discord.gg/jyuC9nuFST"
     )
 
